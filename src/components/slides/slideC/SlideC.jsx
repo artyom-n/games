b504a494683796867ab405c732af4d17d9a859a5
artyom-n <workprompt@gmail.com>
2022-01-02 15:46:36 +0200

    Add SlideC btn

--- src/components/slides/slideC/SlideC.jsx
+++ src/components/slides/slideC/SlideC.jsx
@@ -1,6 +1,7 @@
-// import styles from './SlideC.module.scss';
+import styles from './SlideC.module.scss';
 import TopText from '../../texts/topText/TopText';
 import QuestionTabs from '../../questionTabs/QuestionTabs';
+import BottomTextA from '../../texts/bottomTextA/BottomTextA';
 
 const SlideC = () => {  
 
@@ -12,6 +13,16 @@ const SlideC = () => {
       <TopText
         text={'Which casino game offers you the best statistical chance of winning?'}
       />
+      <BottomTextA 
+        text={'Select the answer to earn (10 Free Spins)'}
+      />
+      <div className={styles.buttonWrapper}>
+            <button
+                className={styles.btnUnactive}                
+            >
+                Next
+            </button>
+        </div>
     </>
   );
 }
